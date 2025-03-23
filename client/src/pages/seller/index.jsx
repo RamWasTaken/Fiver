6c4d7c0b626b2a8e42c73d8ba67839a76ecb2e9b
Ram Kardile <ramkardile4ever@gmail.com>
2025-03-23 13:54:45 +0530

    working code No Image , fix seller dashboard

--- client/src/pages/seller/index.jsx
+++ client/src/pages/seller/index.jsx
@@ -10,15 +10,15 @@ function Index() {
   const [{ userInfo }] = useStateProvider();
   const router = useRouter();
   const [dashboardData, setDashboardData] = useState(undefined);
-  const [cookies] = useCookies(['jwt']); 
+  const [cookies] = useCookies(['jwt']);
   useEffect(() => {
     const getBuyerDashboardData = async () => {
       const response = await axios.get(GET_SELLER_DASHBOARD_DATA, {
         withCredentials: true,
         headers: {
-            Authorization: `Bearer ${cookies.jwt}`, // Add the Authorization header
-          },
-        });
+          Authorization: `Bearer ${cookies.jwt}`, // Add the Authorization header
+        },
+      });
       if (response.status === 200) {
         setDashboardData(response.data.dashboardData);
       }
@@ -37,12 +37,13 @@ function Index() {
               <div>
                 {userInfo?.imageName ? (
                   <Image
-                    src={userInfo.imageName}
+                    src="https://images.unsplash.com/photo-1622804136680-46dc2a8f0598?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="Profile"
                     width={140}
                     height={140}
                     className="rounded-full"
                   />
+
                 ) : (
                   <div className="bg-purple-500 h-24 w-24 flex items-center justify-center rounded-full relative">
                     <span className="text-5xl text-white">
@@ -77,7 +78,7 @@ function Index() {
                 className="shadow-md h-max p-10 flex flex-col gap-2 cursor-pointer hover:shadow-xl transition-all duration-300"
                 onClick={() => router.push("/seller/orders")}
               >
-                <h2 className="text-xl">Total Orders</h2>
+                <h2 className="text-xl">Waiting call</h2>
                 <h3 className="text-[#1DBF73] text-3xl font-extrabold">
                   {dashboardData?.orders}
                 </h3>
@@ -93,19 +94,19 @@ function Index() {
               </div>
 
               <div className="shadow-md h-max p-10 flex flex-col gap-2 cursor-pointer hover:shadow-xl transition-all duration-300">
-                <h2 className="text-xl">Earnings Today</h2>
+                <h2 className="text-xl">Today-Merit</h2>
                 <h3 className="text-[#1DBF73] text-3xl font-extrabold">
                   ${dashboardData?.dailyRevenue}
                 </h3>
               </div>
               <div className="shadow-md h-max p-10 flex flex-col gap-2 cursor-pointer hover:shadow-xl transition-all duration-300">
-                <h2 className="text-xl">Earnings Monthly</h2>
+                <h2 className="text-xl">Rating-Monthly</h2>
                 <h3 className="text-[#1DBF73] text-3xl font-extrabold">
                   ${dashboardData?.monthlyRevenue}
                 </h3>
               </div>
               <div className="shadow-md h-max p-10 flex flex-col gap-2 cursor-pointer hover:shadow-xl transition-all duration-300">
-                <h2 className="text-xl">Earnings Yearly</h2>
+                <h2 className="text-xl">Total-Stars</h2>
                 <h3 className="text-[#1DBF73] text-3xl font-extrabold">
                   ${dashboardData?.revenue}
                 </h3>
