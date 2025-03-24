608590ec3e661b1c6806df9b39b891110c3cf3ab
Ram Kardile <ramkardile4ever@gmail.com>
2025-03-24 19:54:33 +0530

    why clerk was there ? removed it from _app.jsx

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,15 +1,13 @@
 import "../globals.css";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
 import { useEffect } from "react";
 import { StateProvider } from "../context/StateContext";
 import reducer, { initialState } from "../context/StateReducers";
-
-// Import ClerkProvider
-import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -26,23 +24,22 @@ export default function App({ Component, pageProps }) {
   }, [cookies, router]);
 
   return (
-    <ClerkProvider>
-      <StateProvider initialState={initialState} reducer={reducer}>
-        <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <title>Skill Share</title>
-        </Head>
-        <div className="relative flex flex-col h-screen justify-between">
-          <Navbar />
-          <div
-            className={`${router.pathname !== "/" ? "mt-36" : ""
-              } mb-auto w-full mx-auto`}
-          >
-            <Component {...pageProps} />
-          </div>
-          <Footer />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Skill Share</title>
+      </Head>
+      <div className="relative flex flex-col h-screen justify-between">
+        <Navbar />
+        <div
+          className={`${
+            router.pathname !== "/" ? "mt-36" : ""
+          } mb-auto w-full mx-auto`}
+        >
+          <Component {...pageProps} />
         </div>
-      </StateProvider>
-    </ClerkProvider>
+        <Footer />
+      </div>
+    </StateProvider>
   );
 }
