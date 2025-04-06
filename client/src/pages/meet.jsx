727046b273c3c4c063ac943f8065301cb6ec5844
Ram Kardile <ramkardile4ever@gmail.com>
2025-04-06 19:25:15 +0530

    checkout->meet page work , now on meet up button send to personal meeting room of welovephysics

--- client/src/pages/meet.jsx
+++ client/src/pages/meet.jsx
@@ -2,7 +2,7 @@ export default function MeetPage() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-3xl mb-4">Join a Video Meeting</h1>
-      <a href="https://video-call-tau-orcin.vercel.app/sign-in" target="_blank" rel="noopener noreferrer">
+      <a href="https://video-call-tau-orcin.vercel.app/meeting/user_2qsoU6yQbRELTztLAz7D37lhqp1?personal=true" target="_blank" rel="noopener noreferrer">
         <button className="px-6 py-3 bg-blue-500 rounded-lg text-lg">
           Go to Zoom-Clone
         </button>
@@ -10,3 +10,5 @@ export default function MeetPage() {
     </div>
   );
 }
+
+// link to personal room of user1 : video-call-tau-orcin.vercel.app/meeting/user_2qsoU6yQbRELTztLAz7D37lhqp1?personal=true
